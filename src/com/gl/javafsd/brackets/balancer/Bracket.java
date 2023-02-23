42d9ac31f809e26707d2e0ede0efa67261a7871e
arun2196 <arun.tiwari2196@gmail.com>
2023-02-23 16:07:47 +0530

    Update Bracket.java

--- src/com/gl/javafsd/brackets/balancer/Bracket.java
+++ src/com/gl/javafsd/brackets/balancer/Bracket.java
@@ -1,2 +1,26 @@
-package com.gl.javafsd.brackets.balancer;public class Bracket {
+package com.gl.javafsd.brackets.balancer;
+
+public class Bracket {
+
+    private Character openChar;
+    private Character closeChar;
+
+    public Bracket(Character openChar, Character closeChar) {
+
+        this.openChar = openChar;
+        this.closeChar = closeChar;
+    }
+
+    public Character getOpenChar() {
+        return openChar;
+    }
+    public void setOpenChar(Character openChar) {
+        this.openChar = openChar;
+    }
+    public Character getCloseChar() {
+        return closeChar;
+    }
+    public void setCloseChar(Character closeChar) {
+        this.closeChar = closeChar;
+    }
 }
